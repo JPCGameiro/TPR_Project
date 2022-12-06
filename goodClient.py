53a65f374c5a8f1af2431f2f4473055a1617fe01
joao.gameiro@ua.pt <joao.gameiro@ua.pt>
2022-12-06 00:19:36 +0000

    Fix iin good client

--- goodClient.py
+++ goodClient.py
@@ -1,108 +1,98 @@
-import spur
+from paramiko import SSHClient
+import sys
 import time
 
 #Auxiliary function to run algogithms
-def test_algs(shell, dir, program_list):
-	shell.run(["cd", dir])
-	for p in program_list:
-		shell.run(["python3", p])
-	shell.run(["cd", ".."])
+def test_algs(ssh, dir, program_list):
+    stdin, stdout, stderr = ssh.exec_command('ls -l; cd Project/NotImportantFiles/data-structures-algorithms-python-master/algorithms/'+str(dir)+'; ls')
+    print(stdout.read())
+    
+    for p in program_list:
+        stdin, stdout, stderr = ssh.exec_command('ls -l; cd Project/NotImportantFiles/data-structures-algorithms-python-master/algorithms/'+str(dir)+"; python3 "+str(p)+";")
+        print(stdout.read())  
 
 
 
-#Create a python file and put hello world code inside it
-def good_behaviour1():
+#Write a hello world file
+def good_behaviour1(ssh):
     #Create the connection
-    shell = spur.SshShell(hostname="192.168.1.1", username="labcom", password="labcom")
+    ssh.connect('192.168.1.1', username="labcom", password="labcom")
     
-    #delay random
-    shell.run(["ls","-l"])
-    #delay random
-    shell.run(["ls", "Project/"])
-    #delay random
-    shell.run(["cd", "Project/"])
-    #delay random
-    shell.run(["cd", "NotImportantFiles/"])
-    #delay random
-    shell.run(["ls", "-l"])
-    #delay random
-    shell.run(["rm", "-rf", "helloworld.py"])
-    #delay random
-    shell.run(["touch", "helloworld.py"])
-    #delay random
-    shell.run(["echo",">>", "print('hello world')", "helloworld.py"])
-    #delay random
-    shell.run(["cat", "helloworld.py"])
-    #delay random
-    shell.run(["python3", "helloworld.py"])
-    #delay random
-    shell.run(["echo", ">", "",""])
-    #delay random
-    shell.run(["cat", "helloworld.py"])
-    #delay random
-    shell.run(["python3 helloworld.py"])
-    #delay random
-    shell.run(["cd", ".."])
-    #delay random
-    shell.run(["cd", ".."])
-    #delay random
-    
-    #Close the connection
-    shell.close()
+    stdin, stdout, stderr = ssh.exec_command('ls -l; cd Project/; ls -l; cd NotImportantFiles/; ls -l')
+    print(stdout.read())
+    #Random Delay
 
+    stdin, stdout, stderr = ssh.exec_command('ls -l; cd Project/; ls -l; cd NotImportantFiles/; ls -l; rm -rf helloworld.py')
+    print(stdout.read())
+    #Random Delay
 
+    stdin, stdout, stderr = ssh.exec_command('cd Project/NotImportantFiles/; touch helloworld.py')
+    print(stdout.read())
+    #Random Delay
+ 
+    stdin, stdout, stderr = ssh.exec_command('cd Project/NotImportantFiles/; ls; echo "print(\'helloFworld\')" >> helloworld.py')
+    print(stdout.read())
+    #Random Delay
 
-#Run algorithm tests
-def good_behaviour2(shell):
-    #Create the connection
-    shell = spur.SshShell(hostname="192.168.1.1", username="labcom", password="labcom")
-
-    shell.run(["cd", "Project"])
-    shell.run(["cd", "NotImportantFiles"])
-    shell.run(["cd", "data-structures-algorithms-python-master"])
-    shell.run(["cd", "algorithms"])
-    
-    test_algs(shell, "1_BinarySearch", ["binary_search_exercise_solution.py", "binarysearch.py"])
-    test_algs(shell, "2_BubbleSort", ["bubble_sort_exercise_solution.py", "bubble_sort.py"])
-    test_algs(shell, "3_QuickSort", ["quick_sort_exercise_soluiton_lomuto.py", "quick_sort.py"])
-    test_algs(shell, "4_InsertionSort", ["insertion_sort_exercise_solution.py", "insertion_sort.py"])
-    test_algs(shell, "5_MergeSort", ["merge_sort_exercise_solution.py", "merge_sort_primitive.py", "merge_sort_final.py"])
-    test_algs(shell, "6_ShellSort", ["shell_sort.py", "shell_sort_exercise_solution.py"])
-    test_algs(shell, "7_SelectionSort", ["selection_sort_exercise_solution.py", "selection_sort.py"])
-    test_algs(shell, "8_DepthFirstSearch", ["dfs_exercise.py", "dfs.py"])
-    test_algs(shell, "8_recursion", ["recursion.py"])
-    test_algs(shell, "9_BreadthFirstSearch", ["bfs.py", "bfs_exercise_solution.py"])
-
-    shell.run(["cd", "../../../.."])
+    stdin, stdout, stderr = ssh.exec_command('cd Project/NotImportantFiles/; python3 helloworld.py')
+    print(stdout.read())
+    #Random Delay
 
     #Close the connection
-    shell.close()
+    ssh.close()
 
 
 
-def good_behavior3():
+
+
+def good_behaviour2(ssh):
     #Create the connection
-    shell = spur.SshShell(hostname="192.168.1.1", username="labcom", password="labcom")
+    ssh.connect('192.168.1.1', username="labcom", password="labcom")
+    
+    stdin, stdout, stderr = ssh.exec_command('ls -l; cd Project/; ls -l; cd NotImportantFiles/; ls -l')
+    print(stdout.read())
+    #Random Delay
+    
+    stdin, stdout, stderr = ssh.exec_command('ls -l; cd Project/NotImportantFiles/data-structures-algorithms-python-master/algorithms; ls -l')
+    print(stdout.read())
+    #Random Delay   
+    
+    
+    test_algs(ssh, "1_BinarySearch", ["binary_search_exercise_solution.py", "binarysearch.py"])
+    print("1 Done")
+    test_algs(ssh, "2_BubbleSort", ["bubble_sort_exercise_solution.py", "bubble_sort.py"])
+    print("2 Done")
+    test_algs(ssh, "3_QuickSort", ["quick_sort_exercise_soluiton_lomuto.py", "quick_sort.py"])
+    print("3 Done")
+    test_algs(ssh, "4_InsertionSort", ["insertion_sort.py"])
+    print("4 Done")
+    test_algs(ssh, "5_MergeSort", ["merge_sort_exercise_solution.py", "merge_sort_primitive.py", "merge_sort_final.py"])
+    print("5 Done")
+    test_algs(ssh, "6_ShellSort", ["shell_sort.py", "shell_sort_exercise_solution.py"])
+    print("6 Done")
+    test_algs(ssh, "7_SelectionSort", ["selection_sort_exercise_solution.py", "selection_sort.py"])
+    print("7 Done")
+    test_algs(ssh, "8_DepthFirstSearch", ["dfs_exercise.py", "dfs.py"])
+    print("8 Done")
+    test_algs(ssh, "8_recursion", ["recursion.py"])
+    print("9 Done")
+    test_algs(ssh, "9_BreadthFirstSearch", ["bfs.py", "bfs_exercise_solution.py"])
+    print("All Done")
 
-    shell.run(["ip", "a"])
-    shell.run(["lscpu"])
-    shell.run(["ps", "aux"])
+    #shell.run(["cd", "../../../.."])
 
     #Close the connection
-    shell.close()
+    ssh.close()
+
+def good_behavior3(shell):
+    pass
 
 
 
 
+#Create SSH client
+ssh = SSHClient()
+ssh.load_system_host_keys()
 
-#Execute commands
-with shell:
-    result = shell.run(["echo", "-n", "hello"])
-    time.sleep(2)
-    print(result.output)
-    result = shell.run(["ls", "-l"])
-    time.sleep(2)
-    print(result.output)
-    result = shell.run(["echo", "test"])
-    time.sleep(2)
-
+#good_behaviour1(ssh)
+good_behaviour2(ssh)

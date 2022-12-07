a7b7ed5f056a9fdbfc29b564278d24f488e98c3e
JPCGameiro <joao.gameiro@ua.pt>
2022-12-07 18:46:30 +0000

    Código cringe

--- goodClient.py
+++ goodClient.py
@@ -1,6 +1,7 @@
 from paramiko import SSHClient
 import sys
 import time
+from random import randrange
 
 #Auxiliary function to run algogithms
 def test_algs(channel, dir, program_list):
@@ -136,26 +137,57 @@ def good_behaviour2(ssh):
     out = channel.recv(9999)
     print(out.decode("ascii"))
     
-    test_algs(channel, "1_BinarySearch", ["binary_search_exercise_solution.py", "binarysearch.py"])
-    print("1 Done")
-    test_algs(channel, "2_BubbleSort", ["bubble_sort_exercise_solution.py", "bubble_sort.py"])
-    print("2 Done")
-    test_algs(channel, "3_QuickSort", ["quick_sort_exercise_soluiton_lomuto.py", "quick_sort.py"])
-    print("3 Done")
-    test_algs(channel, "4_InsertionSort", ["insertion_sort.py"])
-    print("4 Done")
-    test_algs(channel, "5_MergeSort", ["merge_sort_exercise_solution.py", "merge_sort_primitive.py", "merge_sort_final.py"])
-    print("5 Done")
-    test_algs(channel, "6_ShellSort", ["shell_sort.py", "shell_sort_exercise_solution.py"])
-    print("6 Done")
-    test_algs(channel, "7_SelectionSort", ["selection_sort_exercise_solution.py", "selection_sort.py"])
-    print("7 Done")
-    test_algs(channel, "8_DepthFirstSearch", ["dfs_exercise.py", "dfs.py"])
-    print("8 Done")
-    test_algs(channel, "8_recursion", ["recursion.py"])
-    print("9 Done")
-    test_algs(channel, "9_BreadthFirstSearch", ["bfs.py", "bfs_exercise_solution.py"])
-    print("All Done")
+    
+    
+    i = randrange(10)
+    if (i <= 3):
+        test_algs(channel, "1_BinarySearch", ["binary_search_exercise_solution.py", "binarysearch.py"])
+        #Random delay
+        test_algs(channel, "2_BubbleSort", ["bubble_sort_exercise_solution.py", "bubble_sort.py"])
+        #Random delay
+        test_algs(channel, "3_QuickSort", ["quick_sort_exercise_soluiton_lomuto.py", "quick_sort.py"])
+    elif ( i > 3 and i < 8 )
+        test_algs(channel, "4_InsertionSort", ["insertion_sort.py"])
+        #Random delay
+        test_algs(channel, "5_MergeSort", ["merge_sort_exercise_solution.py", "merge_sort_primitive.py", "merge_sort_final.py"])
+        #Random delay
+        test_algs(channel, "6_ShellSort", ["shell_sort.py", "shell_sort_exercise_solution.py"])
+        #Random delay
+        test_algs(channel, "7_SelectionSort", ["selection_sort_exercise_solution.py", "selection_sort.py"])
+    else:
+        test_algs(channel, "8_DepthFirstSearch", ["dfs_exercise.py", "dfs.py"])
+        #Random delay
+        test_algs(channel, "8_recursion", ["recursion.py"])
+        #Random delay
+        test_algs(channel, "9_BreadthFirstSearch", ["bfs.py", "bfs_exercise_solution.py"])
+
+
+    i = random.uniform(0, 1)
+    while (i < 0.5):
+        i = randrange(10)
+        if (i == 0):
+            test_algs(channel, "1_BinarySearch", ["binary_search_exercise_solution.py", "binarysearch.py"])
+        elif (i==1):
+            test_algs(channel, "2_BubbleSort", ["bubble_sort_exercise_solution.py", "bubble_sort.py"])
+        elif (i==2):
+            test_algs(channel, "3_QuickSort", ["quick_sort_exercise_soluiton_lomuto.py", "quick_sort.py"])
+        elif (i==3):
+            test_algs(channel, "4_InsertionSort", ["insertion_sort.py"])
+        elif (i==4):
+            test_algs(channel, "5_MergeSort", ["merge_sort_exercise_solution.py", "merge_sort_primitive.py", "merge_sort_final.py"])
+        elif (i==5):
+            test_algs(channel, "6_ShellSort", ["shell_sort.py", "shell_sort_exercise_solution.py"])
+        elif (i==6):
+            test_algs(channel, "7_SelectionSort", ["selection_sort_exercise_solution.py", "selection_sort.py"])
+        elif (i==7):
+            test_algs(channel, "8_DepthFirstSearch", ["dfs_exercise.py", "dfs.py"])
+        elif (i==8):
+            test_algs(channel, "8_recursion", ["recursion.py"])
+        elif (i==9):
+            test_algs(channel, "9_BreadthFirstSearch", ["bfs.py", "bfs_exercise_solution.py"])
+        #Random delay
+        i = random.uniform(0, 1)
+
 
     #Close the connection
     ssh.close()
@@ -222,11 +254,23 @@ def good_behaviour3(ssh):
 
 
 
+
+
+
+
 #Create SSH client
 ssh = SSHClient()
 ssh.load_system_host_keys()
 
-#good_behaviour1(ssh)
-#good_behaviour2(ssh)
-good_behaviour3(ssh)
+i = random.uniform(0, 1)
+while (i < 0.5):
+    i = randrange(3)
+    if i == 0:
+        good_behaviour1(ssh)
+    elif i == 1:
+        good_behaviour2(ssh)
+    elif i == 2:
+        good_behaviour3(ssh)
+    i = random.uniform(0, 1)
+
 

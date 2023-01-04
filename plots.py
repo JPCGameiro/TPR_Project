dfd669c0eee86fe7d253beef8a1fefb2bdbc8126
JPCGameiro <joao.gameiro@ua.pt>
2023-01-04 14:45:42 +0000

    CENakofemld

--- plots.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-
-data= np.loadtxt("Captures/attacker_d1_obs_features.dat", dtype=int)
-plt.plot(data[:,0], data[:,1], 'k')
-plt.show()
-plt.plot(data[:,0], data[:,3], 'b')
-plt.show()
